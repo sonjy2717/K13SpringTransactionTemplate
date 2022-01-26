513bb3a0a1383eaba802421c0d1bbdd4b92a1c67
sonjy2717 <sonjy2717@naver.com>
2022-01-26 09:55:38 +0900

    트랜잭션 템플릿

---
+++ src/main/java/com/kosmo/transacitontemplate/TransactionController.java
@@ -0,0 +1,47 @@
+package com.kosmo.transacitontemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import transaction.TicketDTO;
+import transaction.TicketTplDAO;
+
+@Controller
+public class TransactionController {
+	
+	//트랜잭션 템플릿 사용을 위한 빈 자동주입
+	private TicketTplDAO daoTpl;
+	@Autowired
+	public void setDaoTpl(TicketTplDAO daoTpl) {
+		this.daoTpl = daoTpl;
+	}
+	
+	
+	
+	//티켓구매 페이지
+	@RequestMapping("/transaction/buyTicketTpl.do")
+	public String buyTicketTpl() {
+		return "09Transaction/buyTicketTpl";
+	}
+	
+	//티켓 구매 트랜잭션 처리
+	@RequestMapping("/transaction/buyTicketTplAction.do")
+	public String buyTicketTplAction(TicketDTO ticketDTO, Model model) {
+		
+		//폼값을 한번에 받아서 DAO로 전달(커맨드 객체)
+		boolean isBool = daoTpl.buyTicket(ticketDTO);
+		if (isBool == true) {
+			model.addAttribute("successOrFail", "티켓구매가 정상처리 되었습니다.");
+		}
+		else {
+			model.addAttribute("successOrFail", "티켓구매가 취소 되었습니다. 다시 시도해 주세요.");
+		}
+		
+		//뷰로 전달할 데이터 저장
+		model.addAttribute("ticketInfo", ticketDTO);
+		
+		return "09Transaction/buyTicketTplAction";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<!-- 컨트롤러 : TransactionController.java -->
+	<h3>트랜잭션(Transaction)</h3>
+	<li>
+		<a href="transaction/buyTicketTpl.do">
+			티켓구매하기2(트랜잭션 템플릿 사용)
+		</a>
+	</li>
+</body>
+</html>
